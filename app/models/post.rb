e444cd8faca07ec5e45dc4e4eb4df917a7034d86
davidrox26 <hellpaso26@gmail.com>
2016-08-24 05:33:36 +0000

    Added Post Function

--- app/models/post.rb
+++ app/models/post.rb
@@ -2,6 +2,6 @@ class Post < ActiveRecord::Base
   belongs_to :user
   validates :user_id, presence: true
   validates :content, presence: true
-  default_scope -> { order(created_at: :desc) )
+  default_scope -> { order(created_at: :desc) }
   
 end
